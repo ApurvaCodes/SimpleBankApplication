3cf507dca734d63b58c1ac402ce24ee6e62e2177
Apurva Gawade <apurvagawade29@gmail.com>
2024-12-19 19:36:55 +0530

    Initial commit: Simple Bank Application

---
+++ Simple Bank Application/FirstBank.java
@@ -0,0 +1,10 @@
+package bank_application;
+
+public class FirstBank {
+	
+	public static void main(String[] args) {
+		
+		Account epic = new Account("Epic Statler" , "F000001");
+		epic.showMenu();
+	}
+}
